b439d60d60b5faa10044e9e2b747636a2cb0baa9
Dheshan Mohandass <dheshan.mail@gmail.com>
2022-03-25 15:08:04 -0400

    Fix login UI

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -1,25 +1,43 @@
 import {useNavigate} from "react-router-dom";
-import {useState} from "react";
-import * as service from "../../services/auth-service";
+import {useEffect, useState} from "react";
+import * as service from "../../services/users-service";
+import * as authService from "../../services/auth-service";
+import {UserList} from "./user-list";
 
 export const Login = () => {
+    const [existingUsers, setExistingUsers] = useState([]);
     const [loginUser, setLoginUser] = useState({});
     const navigate = useNavigate()
     const login = () =>
-        service.login(loginUser)
+        authService.login(loginUser)
             .then((user) => navigate('/profile/mytuits'))
             .catch(e => alert(e));
+
+    const deleteUser = (uid) =>
+        service.deleteUser(uid)
+            .then(findAllUsers)
+    const findAllUsers = () =>
+        service.findAllUsers()
+            .then(users => {
+                setExistingUsers(users)
+            })
+    useEffect(findAllUsers, []);
+
     return (
         <div>
             <h1>Login</h1>
-            <input onChange={(e) =>
-                setLoginUser({...loginUser,
-                    username: e.target.value})}/>
-            <input onChange={(e) =>
-                setLoginUser({...loginUser,
-                    password: e.target.value})}/>
-            <button onClick={login}>
-                Login</button>
+            <input className="mb-2 form-control"
+                   onChange={(e) =>
+                       setLoginUser({...loginUser, username: e.target.value})}
+                   placeholder="username"/>
+            <input className="mb-2 form-control"
+                   onChange={(e) =>
+                       setLoginUser({...loginUser, password: e.target.value})}
+                   placeholder="password" type="password"/>
+            <button onClick={login} className="btn btn-primary mb-5">Login</button>
+
+            <h3>Login As</h3>
+            <UserList users={existingUsers} deleteUser={deleteUser}/>
         </div>
     );
 };
