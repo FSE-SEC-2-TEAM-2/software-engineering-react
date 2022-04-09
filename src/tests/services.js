e3c54184ca1bde36d0ce61ed1249683789c81611
shayan_jaleel <shayan.jaleel@gmail.com>
2022-04-09 05:44:59 -0400

    Add test services for follows.

--- src/tests/services.js
+++ src/tests/services.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 export const BASE_URL = process.env.REACT_APP_TEST;
+const LOCAL_BASE_URL = 'http://localhost:4000';
 console.log(`Using ${BASE_URL} for Testing!`);
 
 const LOGIN_API = `${BASE_URL}/login`;
 const USERS_API = `${BASE_URL}/users`;
+const LOCAL_USERS_API = `${LOCAL_BASE_URL}/users`;
 const TUITS_API = `${BASE_URL}/tuits`;
 
 // Users API
@@ -116,3 +118,24 @@ export const userLikesTuit = (uid, tid) =>
 export const userUnlikesTuit = (uid, tid) =>
     axios.delete(`${USERS_API}/${uid}/like/${tid}`)
         .then(response => response.data);
+
+export const followUser = (following_uid, followed_uid) =>
+axios.post(`${LOCAL_USERS_API}/${following_uid}/follows/${followed_uid}`)
+    .then(response => response.data)
+
+export const unfollowUser = (following_uid, followed_uid) =>
+    axios.delete(`${LOCAL_USERS_API}/${following_uid}/follows/${followed_uid}`)
+        .then(response => response.data)
+
+export const isFollowing = (isFollowing_uid, isFollowed_uid) =>{
+    console.log(`${LOCAL_USERS_API}/${isFollowing_uid}/following/${isFollowed_uid}`);
+    return axios.get(`${LOCAL_USERS_API}/${isFollowing_uid}/following/${isFollowed_uid}`)
+        .then(response => response.data)}
+        
+export const findAllFollowersForUser = (followed_uid) =>
+    axios.get(`${LOCAL_USERS_API}/${followed_uid}/followers`)
+        .then(response => response.data)
+                
+export const findAllUsersFollowedByUser = (follower_uid) =>
+    axios.get(`${LOCAL_USERS_API}/${follower_uid}/follows`)
+        .then(response => response.data)
